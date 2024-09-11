c207cb28206efd99304bcda4dfc852b701aceb3b
harish04ss <harishsubramanian4122001@gmail.com>
2024-09-11 11:46:06 +0530

    config

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const {mongoDB_URL,port} = require('./utils/config');
 
 
 
-mongoose.connect(mongoDB_URL).then(()=>{
+mongoose.connect(process.env.mongoDB_URL).then(()=>{
     console.log("connected to DB");
-    app.listen(port,() =>{
-        console.log('server is runing on http://127.0.0.1:${port}')
+    app.listen(process.env.port,() =>{
+        console.log('server is runing on http://127.0.0.1:${process.env.port}')
     });
 }).catch((error)=>{
     console.log("error in connection: ",error);
